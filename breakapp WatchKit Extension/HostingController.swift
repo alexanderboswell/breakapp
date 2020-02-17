f9803c74af8cb88f01b8d03ab935118752cb6b7b
Alex Boswell <alexanderboswell@gmail.com>
2020-02-16 16:55:54 -0800

    Initial Commit

---
+++ breakapp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  breakapp WatchKit Extension
+//
+//  Created by alexander boswell on 2/16/20.
+//  Copyright © 2020 alexander boswell. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ breakapp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  breakapp WatchKit Extension
+//
+//  Created by alexander boswell on 2/16/20.
+//  Copyright © 2020 alexander boswell. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
